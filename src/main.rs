d6c63af0881e4ec1abf0e3d21f5cd1b048e8771d
Edgardo Carreras <me@edgardocarreras.com>
2023-08-23 00:59:46 -0400

    Bug Fix: Add state to app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use crate::game_state::AppState;
 use crate::gold_resource::ResourcesPlugin;
 use crate::ui::UIPlugin;
 
@@ -8,6 +9,7 @@ mod game_state;
 
 fn main() {
     let mut app = App::new();
+    app.add_state::<AppState>();
     app.add_plugins((
         DefaultPlugins,
         UIPlugin,
