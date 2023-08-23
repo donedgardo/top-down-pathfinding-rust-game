a1a19eac303336390ed3875467934eb8f96a6584
Edgardo Carreras <me@edgardocarreras.com>
2023-08-22 21:45:44 -0400

    Feature: Add gold resource struct, with banacnce and gain methods

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use bevy::prelude::*;
+
+struct GoldResource(u32);
+
+impl GoldResource {
+    pub fn gain(&mut self, amount: u32) {
+        self.0 += 5;
+    }
+    pub fn balance(&self) -> u32 {
+        self.0
+    }
+}
+
+#[derive(States, PartialEq, Eq, Debug, Clone, Hash, Default)]
+enum AppState {
+    #[default]
+    InGame,
+}
+
+fn setup_in_game_ui(mut commands: Commands) {
+    commands.spawn((
+        GoldResourceLabel,
+        TextBundle::from_section(
+            "0".to_string(),
+            TextStyle::default(),
+        ))
+    );
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(Component)]
+pub struct GoldResourceLabel;
+
+#[cfg(test)]
+mod resources_test {
+    use bevy::core_pipeline::core_2d::Core2dPlugin;
+    use bevy::prelude::*;
+    use crate::*;
+
+    #[test]
+    fn it_shows_gold_resources_label() {
+        let mut app = setup();
+        assert_eq!(app.world.query::<&GoldResourceLabel>()
+                       .iter(&app.world).len(), 1);
+    }
+
+    #[test]
+    fn it_shows_gold_resources_label_default_0() {
+        let mut app = setup();
+        let text = app.world.query_filtered::<&Text, With<GoldResourceLabel>>()
+            .single(&app.world);
+        assert_eq!(text.sections[0].value, "0");
+    }
+
+
+    #[test]
+    fn it_adds_gold_resource() {
+        let mut gold_resource = GoldResource(0);
+        gold_resource.gain(5);
+        assert_eq!(gold_resource.balance(), 5);
+    }
+
+    fn setup() -> App {
+        let mut app = App::new();
+        app.add_plugins(Core2dPlugin);
+        app.add_systems(OnEnter(AppState::InGame), setup_in_game_ui);
+        app.add_state::<AppState>();
+        app.update();
+        app
+    }
+}
